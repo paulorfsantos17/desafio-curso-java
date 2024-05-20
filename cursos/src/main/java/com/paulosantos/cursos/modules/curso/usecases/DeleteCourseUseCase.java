f2dd76dc3a7a4cdc1cd5aad29bec015725502099
Paulo <paulorfsantos17@gmail.com>
2024-05-20 17:52:23 -0300

    DeleteCourseUseCase

---
+++ cursos/src/main/java/com/paulosantos/cursos/modules/curso/usecases/DeleteCourseUseCase.java
@@ -0,0 +1,24 @@
+package com.paulosantos.cursos.modules.curso.usecases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulosantos.cursos.modules.curso.CourseRepository;
+
+import java.util.UUID;
+
+@Service
+public class DeleteCourseUseCase {
+
+  @Autowired
+  private CourseRepository courseRepository;
+
+  public void execute(UUID id) {
+    this.courseRepository.findById(id).orElseThrow(() -> {
+      throw new RuntimeException("Cannot find course");
+    });
+
+    this.courseRepository.deleteById(id);
+  }
+
+}
