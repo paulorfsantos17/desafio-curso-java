7ac92421f841eac2085a225cee1058b4338b7c90
Paulo <paulorfsantos17@gmail.com>
2024-05-20 17:49:39 -0300

    Feat: add validation and constructor in CourseEntity

--- cursos/src/main/java/com/paulosantos/cursos/modules/curso/CourseEntity.java
+++ cursos/src/main/java/com/paulosantos/cursos/modules/curso/CourseEntity.java
@@ -1,6 +1,9 @@
 package com.paulosantos.cursos.modules.curso;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -18,22 +21,25 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
 @Data
+@Builder
 @Entity(name = "course")
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  @NotBlank
+  @NotBlank(message = "O campo [nome] não pode ser vazio.")
   private String name;
 
-  @NotBlank
+  @NotBlank(message = "O campo [category] não pode ser vazio.")
   private String category;
 
   private Active active;
 
   @CreationTimestamp
-  @Column(name = "updated_at")
+  @Column(name = "created_at")
   private LocalDateTime createAt;
 
   @UpdateTimestamp
