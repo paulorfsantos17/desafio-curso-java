9f52b27c5237195c813441acd03cfec3f652ff95
Paulo <paulorfsantos17@gmail.com>
2024-05-20 14:50:35 -0300

    feat: add CourseEntity

---
+++ cursos/src/main/java/com/paulosantos/cursos/modules/curso/CourseEntity.java
@@ -0,0 +1,43 @@
+package com.paulosantos.cursos.modules.curso;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.paulosantos.cursos.modules.enums.Active;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+@Entity(name = "course")
+public class CourseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @NotBlank
+  private String name;
+
+  @NotBlank
+  private String category;
+
+  private Active active;
+
+  @CreationTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime createAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updateAT;
+}
