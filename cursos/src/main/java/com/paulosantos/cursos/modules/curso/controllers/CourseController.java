70db14928284670a27dd76aa334cabff41e4e618
Paulo <paulorfsantos17@gmail.com>
2024-05-20 18:51:27 -0300

    feat: add router update course

--- cursos/src/main/java/com/paulosantos/cursos/modules/curso/controllers/CourseController.java
+++ cursos/src/main/java/com/paulosantos/cursos/modules/curso/controllers/CourseController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.paulosantos.cursos.modules.curso.CourseEntity;
 import com.paulosantos.cursos.modules.curso.dto.CreateCourseRequestDTO;
 import com.paulosantos.cursos.modules.curso.dto.GetAllCourseResponseDTO;
+import com.paulosantos.cursos.modules.curso.dto.UpdateCourseRequestDTO;
 import com.paulosantos.cursos.modules.curso.enums.Active;
 import com.paulosantos.cursos.modules.curso.usecases.ActiveCourseUseCase;
 import com.paulosantos.cursos.modules.curso.usecases.CreateCourseUseCase;
 import com.paulosantos.cursos.modules.curso.usecases.DeleteCourseUseCase;
 import com.paulosantos.cursos.modules.curso.usecases.GetAllCourseUsecase;
+import com.paulosantos.cursos.modules.curso.usecases.UpdateCourseUsecase;
 
 import java.util.List;
 import java.util.UUID;
@@ -44,6 +47,9 @@ public class CourseController {
 
   @Autowired
   private ActiveCourseUseCase activeCourseUseCase;
+
+  @Autowired
+  private UpdateCourseUsecase updateCourseUseCase;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CreateCourseRequestDTO createCourseRequestDTO) {
@@ -109,4 +115,15 @@ public class CourseController {
 
   }
 
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateCourse(@PathVariable UUID id,
+      @Valid @RequestBody UpdateCourseRequestDTO updateCourseRequestDTO) {
+    try {
+      this.updateCourseUseCase.execute(id, updateCourseRequestDTO);
+      return ResponseEntity.status(HttpStatus.OK).build();
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+  }
 }
